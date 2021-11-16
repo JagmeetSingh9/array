e5fc622056de5d328569e754cc5e378f19085820
JagmeetSingh9 <jagmeetsingh999991@gmail.com>
2021-11-16 16:40:29 +0530

    EQUIWRAP

---
+++ eqwrap.java
@@ -0,0 +1,8 @@
+public class eqwrap {
+    public static void main(String[] args) {
+        Integer a=11;
+        Integer b=12;
+        System.out.println(a.equals(b));
+        System.out.println(a.compareTo(a));
+    }
+}
